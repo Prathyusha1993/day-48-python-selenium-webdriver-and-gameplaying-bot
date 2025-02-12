2cbf977500f39041d6c5d564f83b26430127839c
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-02-11 16:07:09 -0800

    day 48 pyhton challlenge selenium web driver

---
+++ main1.py
@@ -0,0 +1,18 @@
+# another example using name attribute
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# keep browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+driver =  webdriver.Chrome(options=chrome_options)
+driver.get('https://www.python.org/')
+search_bar = driver.find_element(By.NAME, value='q')
+print(search_bar.get_attribute('placeholder'))
+
+button = driver.find_element(By.ID, value='submit')
+print(button.size)
+
+# driver.close()
+driver.quit()
